f6225c790f736802081d2174827daa73c7c2e11f
binourson <binourson@proton.me>
2026-07-02 13:27:31 +0200

    feat: navigation bar on the top of the screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,21 @@ import ProductsPage from "./pages/ProductsPage.jsx";
 import ProductDetailPage from "./pages/ProductDetailPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
+import Navbar from "./components/Navbar.jsx";
 import "./App.css";
 
 // App is now just the ROUTER: it maps each URL path to a page component.
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<ProductsPage />} />
-      <Route path="/products/:id" element={<ProductDetailPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </>
   );
 }
 
